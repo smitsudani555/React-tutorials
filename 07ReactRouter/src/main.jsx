d00a5f3e9114d44bac53e27cd0a1945297e3774e
smitsudani555 <smitsudani555@gmail.com>
2025-02-27 23:10:10 +0530

    context discussion done

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
