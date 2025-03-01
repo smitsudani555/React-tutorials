c633ae27c637b08b5e228023061d874db8bf4730
smitsudani555 <smitsudani555@gmail.com>
2025-03-01 13:44:33 +0530

    configuration done

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+  return (
+    <>
+      <h1>A blog application with appwrite</h1>
+    </>
+  )
+}
+
+export default App
